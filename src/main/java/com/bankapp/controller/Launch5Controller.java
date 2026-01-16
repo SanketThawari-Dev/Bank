e904931909656f740bf13ab28e07ff9a4ef658ae
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-16 18:03:33 +0530

    Add Launch5Controller for withdrawal functionality

---
+++ src/main/java/com/bankapp/controller/Launch5Controller.java
@@ -0,0 +1,46 @@
+package com.bankapp.controller;
+
+import java.sql.Connection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bankapp.config.ConnectionFactory;
+import com.bankapp.dao.AccountDao;
+import com.bankapp.dao.Transac_his_dao;
+import com.bankapp.entity.Account;
+
+@Controller
+public class Launch5Controller {
+
+    Connection con = ConnectionFactory.getCon();
+
+    AccountDao acDao = new AccountDao();
+    Transac_his_dao txnDao = new Transac_his_dao();
+
+    @PostMapping("/withdraw")
+    public String withdraw(HttpServletRequest req) {
+
+        String uid = req.getParameter("uid");
+        String am = req.getParameter("am");
+
+        String res = acDao.withdraw(con, uid, am);
+        txnDao.insertTrans(con, uid, "withdraw", am);
+
+        Account account = acDao.readAccount(con, uid);
+        HttpSession session = req.getSession();
+
+        if ("updated".equals(res)) {
+            session.setAttribute("check", uid);
+            session.setAttribute("ac", account);
+            session.setAttribute("msg", "Money withdraw");
+            return "redirect:/account.jsp";
+        } else {
+            session.setAttribute("msg", "Insufficient funds");
+            return "redirect:/account.jsp";
+        }
+    }
+}
