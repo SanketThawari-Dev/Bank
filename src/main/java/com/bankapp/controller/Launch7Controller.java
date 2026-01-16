398daf49dcf4b81ca0263bb6c3a1042d0c75eb25
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-16 20:33:28 +0530

    Update Launch7Controller.java

--- src/main/java/com/bankapp/controller/Launch7Controller.java
+++ src/main/java/com/bankapp/controller/Launch7Controller.java
@@ -3,8 +3,8 @@ package com.bankapp.controller;
 import java.sql.Connection;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
