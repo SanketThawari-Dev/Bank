b57cf64a1eee3b47f7b8027c19b9de95876b5f21
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-20 19:24:01 +0530

    Update Launch4Controller.java

--- src/main/java/com/bankapp/controller/Launch4Controller.java
+++ src/main/java/com/bankapp/controller/Launch4Controller.java
@@ -1,7 +1,6 @@
 package com.bankapp.controller;
 
 import java.sql.Connection;
-
 import javax.sql.DataSource;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -43,16 +42,18 @@ public class Launch4Controller {
                 session.setAttribute("check", uid);
                 session.setAttribute("ac", account);
                 session.setAttribute("msg", "Money deposited");
-                return "redirect:/account.jsp";
+
+                // ✅ redirect to controller
+                return "redirect:/account";
             } else {
                 session.setAttribute("msg", "Failed");
-                return "redirect:/account.jsp";
+                return "redirect:/account";
             }
 
         } catch (Exception e) {
             e.printStackTrace();
             session.setAttribute("msg", "Internal server error");
-            return "redirect:/account.jsp";
+            return "redirect:/account";
         }
     }
 }
