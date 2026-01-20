bb7c83aa2aedbf4e626e422c47ea7e641610aab8
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-20 19:04:36 +0530

    Update HomeController.java

--- src/main/java/com/bankapp/controller/HomeController.java
+++ src/main/java/com/bankapp/controller/HomeController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    // Landing page
     @GetMapping("/")
-    public String home() {
-        return "login";   // resolves to /WEB-INF/jsp/login.jsp
+    public String landingPage() {
+        return "landing";   // /WEB-INF/jsp/landing.jsp
+    }
+
+    // Login page
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";     // /WEB-INF/jsp/login.jsp
     }
 }
-
