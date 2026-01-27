2d10c4b733e4dd1a22c835f49c2d3a8787349af8
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-27 16:24:21 +0530

    Update TransactionService.java

--- src/main/java/com/bank/service/TransactionService.java
+++ src/main/java/com/bank/service/TransactionService.java
@@ -1,1 +1,27 @@
+package com.bank.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bank.entity.TxnHistory;
+import com.bank.repository.TransactionRepository;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public void insertTxn(String uid, String status, String amount) {
+        transactionRepository.insertTxn(uid, status, amount);
+    }
+
+    public List<TxnHistory> readTxn(String uid) {
+        return transactionRepository.readTxn(uid);
+    }
+}
 
