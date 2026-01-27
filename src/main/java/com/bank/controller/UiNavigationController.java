face753ba1f30eb5f5c289c2349bb245928de17d
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-27 16:04:02 +0530

    Update UiNavigationController.java

--- src/main/java/com/bank/controller/UiNavigationController.java
+++ src/main/java/com/bank/controller/UiNavigationController.java
@@ -1,1 +1,22 @@
+@Controller
+public class UiNavigationController {
+
+    @GetMapping("/loginu")
+    public String loginu(HttpSession session) {
+        session.removeAttribute("msg");
+        return "redirect:/login.jsp";
+    }
+
+    @GetMapping("/signupu")
+    public String signupu(HttpSession session) {
+        session.removeAttribute("msg");
+        return "redirect:/signup.jsp";
+    }
+
+    @GetMapping("/accountu")
+    public String accountu(HttpSession session) {
+        session.removeAttribute("msg");
+        return "redirect:/account.jsp";
+    }
+}
 
