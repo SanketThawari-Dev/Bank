92e95c1f931f36638176c27db4655e27c7653682
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-27 16:24:02 +0530

    Update AccountService.java

--- src/main/java/com/bank/service/AccountService.java
+++ src/main/java/com/bank/service/AccountService.java
@@ -1,1 +1,25 @@
+package com.bank.service;
+
+import org.springframework.stereotype.Service;
+
+import com.bank.entity.Account;
+import com.bank.repository.AccountRepository;
+
+@Service
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public String deposit(String uid, String am) {
+        return accountRepository.deposit(uid, am);
+    }
+
+    public Account readAccount(String uid) {
+        return accountRepository.readAccount(uid);
+    }
+}
 
