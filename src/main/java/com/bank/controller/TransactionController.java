992f9a4a4315db4c9e856c8b6227559fd465d211
SanketThawari-Dev <155759492+SanketThawari-Dev@users.noreply.github.com>
2026-01-27 16:03:48 +0530

    Update TransactionController.java

--- src/main/java/com/bank/controller/TransactionController.java
+++ src/main/java/com/bank/controller/TransactionController.java
@@ -1,1 +1,17 @@
+@Controller
+public class TransactionController {
+
+    private final TransactionService txnService;
+
+    public TransactionController(TransactionService txnService) {
+        this.txnService = txnService;
+    }
+
+    @GetMapping("/readtxn")
+    public String readTxn(@RequestParam("uid") String uid, HttpSession session) {
+        session.setAttribute("check", uid);
+        session.setAttribute("al", txnService.readTxn(uid));
+        return "redirect:/readtxn.jsp";
+    }
+}
 
